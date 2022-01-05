43e777e764be6b67c40e2d13056b68e1a731c8c7
Oben2002 <dorcasoben2002@gmail.com>
2022-01-05 16:30:44 +0100

    database modified

---
+++ ict301.sql
@@ -0,0 +1,202 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jan 05, 2022 at 03:18 PM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ict301`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `adminstratif`
+--
+
+DROP TABLE IF EXISTS `adminstratif`;
+CREATE TABLE IF NOT EXISTS `adminstratif` (
+  `nom` varchar(20) NOT NULL,
+  `tel` bigint(20) NOT NULL,
+  PRIMARY KEY (`nom`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bulletin`
+--
+
+DROP TABLE IF EXISTS `bulletin`;
+CREATE TABLE IF NOT EXISTS `bulletin` (
+  `id_bulletin` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_bulletin`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `classe`
+--
+
+DROP TABLE IF EXISTS `classe`;
+CREATE TABLE IF NOT EXISTS `classe` (
+  `class_id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_pension` varchar(10) NOT NULL,
+  `id_etablissement` varchar(11) NOT NULL,
+  `nom` varchar(10) NOT NULL,
+  `niveau` varchar(10) NOT NULL,
+  PRIMARY KEY (`class_id`),
+  KEY `classe_id_pension_fr` (`id_pension`),
+  KEY `classe_id_etablissement_fr` (`id_etablissement`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `eleve`
+--
+
+DROP TABLE IF EXISTS `eleve`;
+CREATE TABLE IF NOT EXISTS `eleve` (
+  `Nom` varchar(20) NOT NULL,
+  `Matricule` varchar(20) NOT NULL,
+  `pension` varchar(11) NOT NULL,
+  `class_id` int(11) NOT NULL,
+  PRIMARY KEY (`Matricule`),
+  KEY `eleve_id_pension_fr` (`pension`),
+  KEY `eleve_id_class_fr` (`class_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `enseignant`
+--
+
+DROP TABLE IF EXISTS `enseignant`;
+CREATE TABLE IF NOT EXISTS `enseignant` (
+  `tel` bigint(20) NOT NULL,
+  `nom` varchar(20) NOT NULL,
+  `id_matiere` varchar(15) NOT NULL,
+  PRIMARY KEY (`nom`),
+  KEY `enseignant_id_intitule_fr` (`id_matiere`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `etablissement`
+--
+
+DROP TABLE IF EXISTS `etablissement`;
+CREATE TABLE IF NOT EXISTS `etablissement` (
+  `nom` varchar(11) NOT NULL,
+  `numeroEts` int(11) NOT NULL,
+  `ville` varchar(11) NOT NULL,
+  PRIMARY KEY (`nom`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matiere`
+--
+
+DROP TABLE IF EXISTS `matiere`;
+CREATE TABLE IF NOT EXISTS `matiere` (
+  `intitule` varchar(15) NOT NULL,
+  `class_id` int(11) NOT NULL,
+  `id_bulletin` varchar(100) NOT NULL,
+  `coef` int(11) NOT NULL,
+  PRIMARY KEY (`intitule`),
+  KEY `matiere_id_classe_fr` (`class_id`),
+  KEY `matiere_id_bulletin_fr` (`id_bulletin`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notes`
+--
+
+DROP TABLE IF EXISTS `notes`;
+CREATE TABLE IF NOT EXISTS `notes` (
+  `id_note` varchar(11) NOT NULL,
+  `id_matiere` varchar(15) NOT NULL,
+  `Seq1` int(11) NOT NULL,
+  `Seq2` int(11) NOT NULL,
+  `Seq3` int(11) NOT NULL,
+  `Seq4` int(11) NOT NULL,
+  `Seq5` int(11) NOT NULL,
+  `Seq6` int(11) NOT NULL,
+  PRIMARY KEY (`id_note`),
+  KEY `note_intitule_id_fr` (`id_matiere`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pension`
+--
+
+DROP TABLE IF EXISTS `pension`;
+CREATE TABLE IF NOT EXISTS `pension` (
+  `id_pension` varchar(10) NOT NULL,
+  `Inscription` double NOT NULL,
+  `scolarite` double NOT NULL,
+  PRIMARY KEY (`id_pension`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `classe`
+--
+ALTER TABLE `classe`
+  ADD CONSTRAINT `classe_id_etablissement_fr` FOREIGN KEY (`id_etablissement`) REFERENCES `etablissement` (`nom`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `eleve`
+--
+ALTER TABLE `eleve`
+  ADD CONSTRAINT `eleve_id_class_fr` FOREIGN KEY (`class_id`) REFERENCES `classe` (`class_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `enseignant`
+--
+ALTER TABLE `enseignant`
+  ADD CONSTRAINT `enseignant_id_intitule_fr` FOREIGN KEY (`id_matiere`) REFERENCES `matiere` (`intitule`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `matiere`
+--
+ALTER TABLE `matiere`
+  ADD CONSTRAINT `matiere_id_bulletin_fr` FOREIGN KEY (`id_bulletin`) REFERENCES `bulletin` (`id_bulletin`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `notes`
+--
+ALTER TABLE `notes`
+  ADD CONSTRAINT `note_intitule_id_fr` FOREIGN KEY (`id_matiere`) REFERENCES `matiere` (`intitule`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
