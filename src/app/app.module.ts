ace8518fb6262848ac819d4bb7e9950d908b8964
Noe Kenfack <kenfaclnoe@gmail.com>
2022-01-08 08:54:13 +0100

    test resolving somme problems

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,16 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MaterialModule } from 'src/extensions/mdbootstrap.module';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
-import { MDBRootModule } from 'angular-bootstrap-md';
+import { MDBBootstrapModule,MDBRootModule } from 'angular-bootstrap-md';
+import { FooterComponent } from './footer/footer.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    ToolbarComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
