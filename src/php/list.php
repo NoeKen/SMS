d58d4eee8d2fdc73f2377a4ab4745ddb632ccc3c
kameronaf <bikahf5@gmail.com>
2022-01-09 21:17:22 +0100

    success

--- src/php/list.php
+++ src/php/list.php
@@ -1,4 +1,27 @@
 <?php
+    
+    require 'connect.php';
+    error_reporting(E_ERROR);
+    $teacher = [];
+    $sql = "SELECT + FROM ";
 
+    if ($result = mysqli_query($con, $sql)) {
+        $cr = 0;
+        while ($row = mysqli_fetch_assoc($result)) {
+            $teacher[$cr]['id'] = $row['id'];
+            $teacher[$cr]['name'] = $row['name'];
+            $teacher[$cr]['subname'] = $row['subname'];
+            $teacher[$cr]['age'] = $row['age'];
+            $cr++;
+            # code...
+        } 
+        
+        print_r($teacher);
+       // echo json_encode($teacher);
+        # code...
+    }
+    else {
+        http_response_code(404);
+    }
 
 ?>
