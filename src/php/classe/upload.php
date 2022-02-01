4247b446a6a0e0c066a6c70da97c1f79d529ab81
kameronaf <bikahf5@gmail.com>
2022-02-01 10:23:48 +0100

    success

--- src/php/classe/upload.php
+++ src/php/classe/upload.php
@@ -8,14 +8,20 @@
         $request = json_decode($postdata);
 
         $name = mysqli_real_escape_string($con, trim($request->name));
-        $subname = mysqli_real_escape_string($con, trim($request->name));
-        $email = mysqli_real_escape_string($con, trim($request->name));
+        $class_id = mysqli_real_escape_string($con, trim($request->class_id));
+        $id_etablissement = mysqli_real_escape_string($con, trim($request->id_etablissement));
+        $niveau = mysqli_real_escape_string($con, trim($request->niveau));
+        $id_pension = mysqli_real_escape_string($con, trim($request->id_pension));
+        $examen = mysqli_real_escape_string($con, trim($request->examen));
 
 
         $sql = "UPDATE `` SET
-            `` =  '{$name}',
-            `` =  '{$name}',
-            `` =  '{$name}',
+            `name` =  '{$name}',
+            `class_id` =  '{$class_id}',
+            `id_etablissement` =  '{$id_etablissement}',
+            `niveau` =  '{$niveau}',
+            `id_pension` =  '{$id_pension}',
+            `examen` =  '{$examen}',
          WHERE
             `id` = '{$id}'
         LIMIT 1
